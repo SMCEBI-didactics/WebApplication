e594b8422b9da03c5011f92e437c76110f497331
awowra0 <adam.wowra@smcebi.edu.pl>
2021-12-16 22:13:39 +0100

    Aktualizacja setup.py

--- Verify/setup.py
+++ Verify/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="",
-    description="",
-    version="",
-    author="",
-    author_email="",
-    licence="",
+    name="Verify",
+    description="Some Python scripts",
+    version="1.0",
+    author="awowra",
+    author_email="github.com/awowra0",
+    licence="None",
     packages=['Verify'],
     entry_points={
         'console_scripts' : ['verify = Verify.main:main']
