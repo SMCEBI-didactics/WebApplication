846cb638cf9713cd4e5b3c464a32e756290a2e0e
aleksanderKopec <a.kopec.b@gmail.com>
2021-12-16 18:57:43 +0100

    Added docstrings

--- Verify/Verify/verify.py
+++ Verify/Verify/verify.py
@@ -3,10 +3,31 @@ import os
 import random
 
 """
+This module contains function to 
+hash passwords
+
+Examples:
+    
+    ```
+    from Verify import hash_passwd
+    passwd = "123"
+    _salt, _hash = hash_passwd(passwd)
+    print(_salt, _hash)
+    ```
+
 """
 
 def hash_passwd(passwd, n=32):
     """
+    Function used to create salt and hash passwords using
+    sha256 algorithm.
+
+    Args:
+        passwd (str) - password to be hashed
+        n (int) - length of salt used to hash password
+    
+    Returns:
+        (_salt, _hash) (string, string) - tuple containing salt and hashed password
     """
     _salt = os.urandom(n)
     _hash = hashlib.pbkdf2_hmac("sha256", passwd.encode("utf-8"), _salt, 100000)
--- WebApp/WebApp/views.py
+++ WebApp/WebApp/views.py
@@ -2,23 +2,64 @@ from WebApp import app
 from flask import render_template, request
 
 """
+This is a webapp made with flask framework.
+
+Routes:
+    / - shows index.html
+    /api/<var> - shows api.html
+    /login - shows login.html
 """
 
 @app.route("/")
 def home_route():
     """
+    This is the standard route for application.
+    When user connects with root adress ("/"),
+    The application will serve the index.html file
+    in response.
+
+    Routes:
+        / - renders index.html
+    
+    Returns:
+        Rendered flask template
     """
     return render_template("index.html")
 
 @app.route("/api/<var>")
 def api_route(var):
     """
+    When user connects with /api/ route, the
+    site will print the "api.html" site template
+    using the given <var> value.
+
+    Args:
+        var (str): The parameter given by api call 
+    
+    Routes:
+        /api/<var> - renders api.html with the given <var> parameter
+
+    Returns:
+        Rendered flask template
     """
     return render_template("api.html", var=var)
 
 @app.route("/login", methods=["GET", "POST"])
 def login_route():
     """
+    This function renders the "login.html" which
+    is a simple login form. After sending login information
+    using the "log in" button it will display the received information.
+
+    Routes:
+        /login - renders login.html
+
+    HTTPMethods:
+        GET - used to render the site
+        POST - used to send login info from site to backend
+
+    Returns:
+        Rendered flask template
     """
     state = "Unauthenticated"
     if request.method == "POST":
