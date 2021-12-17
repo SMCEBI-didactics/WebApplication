996942a3c015068d75c496734609e6623846be87
Aleksander Kopeć <akopec@INF-VM>
2021-12-17 07:35:56 +0100

    aktualizacja setup.py

--- Verify/setup.py
+++ Verify/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="",
-    description="",
-    version="",
-    author="",
-    author_email="",
-    licence="",
+    name="Verify",
+    description="Verify",
+    version="1.0",
+    author="akopec",
+    author_email="a.kopec.b@gmail.com",
+    licence="None",
     packages=['Verify'],
     entry_points={
         'console_scripts' : ['verify = Verify.main:main']
